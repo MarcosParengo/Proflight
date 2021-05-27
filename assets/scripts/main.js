002d31b8356d188a6f8628f551509c48a5a89b55
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-05-27 19:24:49 -0300

    Seccion de nuestros alumnos nos recomiendan lista

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,27 +7,21 @@ var aver;
 
 $(document).ready(function () {
     var toggled=false;
-    var mediaqueryList = window.matchMedia("(max-width: 992px)");
-    var mediaqueryListAgent = window.matchMedia("(max-width: 1200px)");
     var navbarSupportedContent= $("#navbarSupportedContent")
     var navBarBrandImage= $("#navBarBrandImage")
     var navBarToggler=$("#navBarToggler")
     var navBarTogglerIcon=$("#navBarTogglerIcon")
-    containerAgent = $("#containerAgent")
-    var team = "assets/data/agent.json"
-    containerValores = $("#containerValores")
-    var perfil = "assets/data/perfil.json"
-    containerPropuesta = $("#containerPropuesta")
-    var propuesta = "assets/data/propuesta.json"
-    LoadJson(perfil, 2)
+    containerTestimonios = $("#containerTestimonios")
+    var testimonios = "assets/data/testimonios.json"
+    LoadJson(testimonios, 2)
 
+    //Cambio de navBar brand on over
     var sourceSwap = function () {
         var $this = $(this);
         var newSource = $this.data('alt-src');
         $this.data('alt-src', $this.attr('src'));
         $this.attr('src', newSource);
     }
-
     $(function () {
         navBarBrandImage.hover(sourceSwap, sourceSwap);
     });
@@ -45,141 +39,37 @@ $(document).ready(function () {
             toggled=false;
     }
     })
-})
-
-function LoadJson(url, which,MQ) {
+})   
+function LoadJson(url) {
     $.ajax({
         method: "get",
         url: url,
         dataType: "json"
     }).done(function (data) {
         obj = data
-        switch (which) {
-            case 1:
-                fillContainerAgent(obj,MQ);
-                break
-            case 2:
-                fillContainerValores(obj);
-                break
-            case 3:
-                fillContainerPropuesta(obj,MQ);
-                break
-        }
+        fillContainerTestimonios(obj)
     })
         .fail(function (error) {
             console.log(error)
         })
 }
-function fillContainerPropuesta(obj,MQ) {
-    contador = 0;
-    if(MQ==0){containerPropuesta.html("");
-    obj.forEach(function (obj, index) {
-        containerPropuesta.append(`
-        <div class="col-sm-12 col-md-4 col-xl-4">
-           <div class="block">
-                <div class="row">
-                    <div class="col-4 col-sm-4">
-                        <img src="${obj.image}" class="img-fluid" alt="Responsive image">
-                    </div>
-                    <div class="col-8 col-sm-8">
-                        <h2>${obj.name}</h2>
-                    </div>
+function fillContainerTestimonios(obj) {
+    obj.forEach(function (obj) {
+        containerTestimonios.append(`
+        <div class="col-sm-3 p-4 mt-5">
+            <div class="card h-100">
+                <img src="assets/images/testimonio/profile/${obj.nombre}.png" class="img-fluid" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${obj.nombre}</h5>
+                <p class="hidden testimonio card-text">${obj.testimonioAdelanto}</p>
+                    <p class="show nacionalidad card-text">${obj.origen}<img src="assets/images/testimonio/bandera/${obj.origen}.png" alt="-"></p>
                 </div>
-           </div>
-        </div>`)
-    contador++
-    if (contador == 5) {
-        containerPropuesta.append(`<div class="w-100"></div>`)
-        contador=0;
-    }
-        
-    });}else{containerPropuesta.html("");
-    obj.forEach(function (obj, index) {
-        containerPropuesta.append(`
-        <div class="col-sm-12 col-md-6 col-xl-6">
-           <div class="block">
-                <div class="row">
-                    <div class="col-4 col-sm-4">
-                        <img src="${obj.image}" class="img-fluid" alt="Responsive image">
-                    </div>
-                    <div class="col-8 col-sm-8">
-                        <h2>${obj.name}</h2>
-                    </div>
+                <div class="card-footer">
+                <a class="show" href="#">Ver testimonio</a>
+                <a class="hidden" href="#">continuar leyendo</a>
                 </div>
-           </div>
-        </div>`)
-    });}
+            </div>
+        </div>
+        `)
+    })
 }
-function fillContainerValores(obj) {
-    containerValores.html("");
-    obj.forEach(function (obj, index) {
-        containerValores.append(`<div class=" col-xs-2 col-sm-4 col-md-3  col-xl-2 ">
-                    <article class="agent" >
-                        <img src="${obj.image}"
-                        <div>
-                            <h2>${obj.name}</h2>
-                        </div>
-                    </article>
-                </div>`);
-    });
-}
-function fillContainerAgent(obj,MQ) {
-    containerAgent.html("")
-    contador = 0;
-    if(MQ==0){//Mayor a 1200, construccion compleja
-        obj.forEach(function (obj, index) {
-            containerAgent.append(`<div class=" col-sm-6 col-md-4  col-xl-2 ">
-                        <article class="agent">
-                                <a href=${obj.link}
-                                target="_blank"><img src="${obj.image}"
-                                width="100%"></a>
-                                <h2 style="margin-top:1em">${obj.name}</h2>
-                                <h2>${obj.phone}</h2>
-                        </article>
-                    </div>`)
-            contador++
-            if (contador == 3) {
-                containerAgent.append(`<div class="w-100"></div>`)
-            } else if (contador == 8) {
-                containerAgent.append(`<div class="w-100"></div>`)
-            } else if (contador == 19) {
-                containerAgent.append(`<div class="w-100"></div>`)
-            }
-            /*
-            if(contador==11){
-                containerAgent.append(`<div class="w-100"></div>`)
-                contador=0
-            }
-            */
-            /*
-            if(contador==2){
-                containerAgent.append(`<div class="w-100"></div>`)
-            }else if(contador==13){
-                containerAgent.append(`<div class="w-100"></div>`)  
-            }else if(contador==19){
-                contador=0
-            }
-            */
-            /*
-            if(contador==5){
-                containerAgent.append(`<div class="w-100"></div>`)
-            }else if(contador==11){
-                contador=0
-            }*/
-        })
-    }  else{//menor a 1200, construccion simple
-        console.log("construccion simple")
-        obj.forEach(function (obj, index) {
-            containerAgent.append(`<div class=" col-sm-4 col-md-3  col-xl-2 ">
-                        <article class="agent">
-                                <a href=${obj.link}
-                                target="_blank"><img src="${obj.image}"></a>
-                                <h2 style="margin-top:1em">${obj.name}</h2>
-                                <h2>${obj.phone}</h2>
-                        </article>
-                    </div>`)
-            contador++
-        })
-    }
-}
-
