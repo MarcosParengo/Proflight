bd51da4cca9d6afa5dfb9ceb50466fa812e2ebac
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-06-01 09:30:01 -0300

    Menu categories preparado

---
+++ menú-categories/porQueElegirnos/assets/scripts/main.js
@@ -0,0 +1,32 @@
+var obj = [];
+
+$(document).ready(function () {
+    var toggled=false;
+    var navbarSupportedContent= $("#navbarSupportedContent")
+    var navBarBrandImage= $("#navBarBrandImage")
+    var navBarToggler=$("#navBarToggler")
+    var navBarTogglerIcon=$("#navBarTogglerIcon")
+
+    var sourceSwap = function () {
+        var $this = $(this);
+        var newSource = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', newSource);
+    }
+    $(function () {
+        navBarBrandImage.hover(sourceSwap, sourceSwap);
+    });
+    navBarToggler.click(function () {
+        var algo=navbarSupportedContent.is(":visible")
+        var newSource = navBarTogglerIcon.data('alt-src');
+        navBarTogglerIcon.data('alt-src', navBarTogglerIcon.attr('src'));
+        navBarTogglerIcon.attr('src', newSource);
+        if(toggled==false){
+            navBarToggler.css('background-color', 'rgba(200,200,200,0.5)');
+            toggled=true;
+        }else{
+            navBarToggler.css('background-color', 'rgba(0,0,0,0.0)');
+            toggled=false;
+    }
+    })
+})   
