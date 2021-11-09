88b080910d70ade2759454d9098e7794a167e952
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-11-08 23:01:21 -0300

    Extranjero mobile done

--- menu-categories/extranjero/assets/scripts/main.js
+++ menu-categories/extranjero/assets/scripts/main.js
@@ -1,5 +1,11 @@
 var obj = [];
 
+function toggleInfo(section) {
+    aux = $(`#${section}1`).attr('class')
+    $(`#${section}1`).attr('class', $(`#${section}2`).attr('class'))
+    $(`#${section}2`).attr('class', aux)
+}
+
 $(document).ready(function () {
     var toggled = false;
     var navBar = $("#navBar")
@@ -12,32 +18,34 @@ $(document).ready(function () {
     var right = document.getElementById("right");
     var left = document.getElementById("left");
 
-    var toggled=false
+    var toggled = false
 
-    $("#right").click(function(){
+    $("#right").click(function () {
         swapImage()
     })
-    $("#left").click(function(){
+    $("#left").click(function () {
         swapImage()
     })
 
-    const swapImage=function(){
-        let aux=left.className
-        $("#left").attr('class',right.className)
-        $('#right').attr('class',aux)
+    const swapImage = function () {
+        let aux = left.className
+        $("#left").attr('class', right.className)
+        $('#right').attr('class', aux)
     }
 
+
+
     navbarBrand.click(function () {
-        if(toggled){
+        if (toggled) {
             if ($(window).width() < 576) {
-                $('#navbarSupportedContent').removeClass('show') 
-               $('#navbarSupportedContent').addClass('collapsing') 
-                navBarToggler.css('visibility', 'visible')                
-                navbarBrand.css('visibility', 'hidden')
+                $('#navbarSupportedContent').removeClass('show')
+                $('#navbarSupportedContent').addClass('collapsing')
+                navBarToggler.css('visibility', 'visible')
+                //navbarBrand.css('visibility', 'hidden')
                 navBar.css('background-color', 'rgba(200,200,200,0.0)');
                 navBar.css('backdrop-filter', 'unset');
                 navBar.css('-webkit-backdrop-filter', 'unset');
-                toggled=!toggled
+                toggled = !toggled
             }
         }
     })
@@ -63,7 +71,7 @@ $(document).ready(function () {
         } else {
             if ($(window).width() < 576) {
                 navBarToggler.css('visibility', 'visible')
-                navbarBrand.css('visibility', 'hidden')
+                //navbarBrand.css('visibility', 'hidden')
                 navBar.css('background-color', 'rgba(200,200,200,0.0)');
                 navBar.css('backdrop-filter', 'unset');
                 navBar.css('-webkit-backdrop-filter', 'unset');
@@ -78,4 +86,48 @@ $(document).ready(function () {
             toggled = !toggled;
         }
     })
-})   
+
+    $(window).bind("scroll", function () {
+        if ($(this).scrollTop() > $('#coverRecibimiento').offset().top) {
+            if ($('#coverRecibimiento').hasClass('beforeScroll')) {
+                $('#coverRecibimiento').removeClass('beforeScroll')
+                $('#coverRecibimiento').addClass('afterScroll')
+            }
+        } else {
+            if ($('#coverRecibimiento').hasClass('afterScroll')) {
+                $('#coverRecibimiento').addClass('beforeScroll')
+                $('#coverRecibimiento').removeClass('afterScroll')
+            }
+        }
+        if ($(this).scrollTop() > $('#coverAlojamiento').offset().top) {
+            if ($('#coverAlojamiento').hasClass('beforeScroll')) {
+                $('#coverAlojamiento').removeClass('beforeScroll')
+                $('#coverAlojamiento').addClass('afterScroll')
+            }
+            if ($('#alojamientosDescripcion').hasClass('beforeScroll')) {
+                $('#alojamientosDescripcion').removeClass('beforeScroll')
+                $('#alojamientosDescripcion').addClass('afterScroll')
+            }
+        } else {
+            if ($('#coverAlojamiento').hasClass('afterScroll')) {
+                $('#coverAlojamiento').addClass('beforeScroll')
+                $('#coverAlojamiento').removeClass('afterScroll')
+            }
+            if ($('#alojamientosDescripcion').hasClass('afterScroll')) {
+                $('#alojamientosDescripcion').addClass('beforeScroll')
+                $('#alojamientosDescripcion').removeClass('afterScroll')
+            }
+        }
+        if ($(this).scrollTop() > $('#landingTitle').offset().top) {
+            if ($('#circle').hasClass('beforeScroll')) {
+                $('#circle').removeClass('beforeScroll')
+                $('#circle').addClass('afterScroll')
+            }
+        }else{
+            if ($('#circle').hasClass('afterScroll')) {
+                $('#circle').addClass('beforeScroll')
+                $('#circle').removeClass('afterScroll')
+            }
+        }
+    });
+})
